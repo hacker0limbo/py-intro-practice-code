ba0282e766e930147ddd44732b9167bd09752136
Stephen <2655461060@qq.com>
2018-10-04 23:21:44 +1000

    add Trigger class as super class

--- tkinter/colour_pick.py
+++ tkinter/colour_pick.py
@@ -5,32 +5,21 @@ and event handling.
 
 __copyright__ = "Stephen"
 
+"""
+总结思考:
+    1, Controls 和 InputCommand 都继承自 tk.Frame, 同时又都有相同的 change_colour() 和 change_text() 方法
+        可以考虑抽出形成一个新类, 这个新基类继承 tk.Frame, Controls 和 InputCommand 继承这个心类
+    2, 一个类里面如果使用 self.xxx 来声明一个部件, 说明这个部件是这个类的一部分(属性), 可以访问并且修改这个部件的配置
+        如果单纯的在 __init__() 里面声明部件, 初始化以后就无法修改. 
+"""
 
 import tkinter as tk
 from tkinter import messagebox
 
-
-class Controls(tk.Frame):
+class Trigger(tk.Frame):
     def __init__(self, master, target_widget):
         super().__init__(master)
         self._target_widget = target_widget
-        self._colors = {
-            'green': 'green',
-            'blue': 'blue',
-        }
-        btn1 = tk.Button(self, text="Change to Green")
-        btn1.pack(side=tk.LEFT, pady=10)
-        btn1.bind("<Button-1>", self.change_green)
-
-        btn2 = tk.Button(self, text="Change to Blue")
-        btn2.pack(side=tk.LEFT, pady=10)
-        btn2.bind("<Button-1>", self.change_blue)
-    
-    """
-    抽出 change_colour 和 change_text, 对所有 button 适用
-    方法:
-        定义一个新的基类(继承自 frame), 让 Controls 和 InputCommand 继承这个类
-    """
 
     def change_colour(self, bg_colour):
         self._target_widget.config(bg=bg_colour)
@@ -39,23 +28,37 @@ class Controls(tk.Frame):
         base_content = f'The table is {content}' 
         self._target_widget.config(text=base_content)
 
+
+class Controls(Trigger):
+    def __init__(self, master, target_widget):
+        super().__init__(master, target_widget)
+        self._colors = {
+            'green': 'green',
+            'blue': 'blue',
+        }
+
+        btn1 = tk.Button(self, text="Change to Green")
+        btn1.pack(side=tk.LEFT, pady=10)
+        btn1.bind("<Button-1>", self.change_green)
+
+        btn2 = tk.Button(self, text="Change to Blue")
+        btn2.pack(side=tk.LEFT, pady=10)
+        btn2.bind("<Button-1>", self.change_blue)
+    
     def change_green(self, event):
-        # target = event.widget
         bg_colour = self._colors['green']
         self.change_colour(bg_colour)
         self.change_text(bg_colour)
 
     def change_blue(self, event):
-        # target = event.widget
         bg_colour = self._colors['blue']
         self.change_colour(bg_colour)
         self.change_text(bg_colour)
 
 
-class InputCommand(tk.Frame):
+class InputCommand(Trigger):
     def __init__(self, master, target_widget):
-        super().__init__(master)
-        self._target_widget = target_widget
+        super().__init__(master, target_widget)
 
         label = tk.Label(self, text='Change the colour to: ')
         label.pack(side=tk.LEFT)
@@ -67,31 +70,16 @@ class InputCommand(tk.Frame):
         btn3.pack(side=tk.LEFT)
         btn3.bind("<Button-1>", self.modify)
 
-    def change_colour(self, bg_colour):
-        self._target_widget.config(bg=bg_colour)
-
-    def change_text(self, content):
-        base_content = f'The table is {content}' 
-        self._target_widget.config(text=base_content)
-
     def modify(self, event):
+        """
+        当输入的内容不是有效的 color 时触发 tk.TclError 错误
+        """
         input_colour = self._entry.get()
         try:
             self.change_colour(input_colour)
             self.change_text(input_colour)
         except tk.TclError:
             messagebox.showerror('Invalid Colour', f'{input_colour} is not a colour')
-
-
-"""
-下面这种写法将 screen 写成了一个类, 里面的属性 _lbl 是一个部件,  因此要通过 screen._lbl 来访问
-另外, 如果是普通的在 init 里面声明一个组件, 如上面的 Controls, 那么只是在初始化就有了, 无法进行外部访问进行改变
-"""
-# class Screen():
-#     def __init__(self, master):
-#         self._master = master
-#         self._lbl = tk.Label(self._master, text="The table is")
-#         self._lbl.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
 
 
 class SampleApp:
@@ -111,7 +99,7 @@ class SampleApp:
         inputCommand.config(pady=10)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     root = tk.Tk()
     app = SampleApp(root)
     root.mainloop()
