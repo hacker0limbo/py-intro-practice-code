c612b62888ee0c531e200819938dd135bbcf1cdf
Stephen <stephen.yin@outlook.com>
2019-01-25 20:51:21 +1000

    update

--- pydash/main/array.py
+++ pydash/main/array.py
@@ -108,3 +108,240 @@ def drop_right(array, n=1):
     :return:
     """
     return array[:-n]
+
+
+def drop_right_while(array, predicate=None):
+    """
+    创建一个切片数组，去除array中从 predicate 返回假值开始到尾部的部分。
+    predicate 会传入3个参数： (value, index, array)
+    :param array:
+    :param predicate:
+    :return:
+    """
+    for i, v in enumerate(array):
+        # 判断传入的参数的数量
+        if predicate.__code__.co_argcount == 1:
+            if predicate(v):
+                return array[:i]
+        elif predicate.__code__.co_argcount == 2:
+            if predicate(v, i):
+                return array[:i]
+        else:
+            if predicate(v, i, array):
+                return array[:i]
+
+
+def drop_while(array, predicate):
+    """
+    创建一个切片数组，去除array中从起点开始到 predicate 返回假值结束部分
+    predicate 会传入3个参数： (value, index, array)
+    :param array:
+    :param predicate:
+    :return:
+    """
+    for i, v in enumerate(array):
+        # 判断传入的参数的数量
+        if predicate.__code__.co_argcount == 1:
+            if not predicate(v):
+                return array[i:]
+        elif predicate.__code__.co_argcount == 2:
+            if not predicate(v, i):
+                return array[i:]
+        else:
+            if not predicate(v, i, array):
+                return array[i:]
+
+
+def fill(array, value, start=0, end=-1):
+    """
+    使用 value 值来填充（替换） array，从start位置开始, 到end位置结束（但不包含end位置）
+    这个方法会改变 array
+    :param array:
+    :param value:
+    :param start:
+    :param end:
+    :return:
+    """
+    if end == -1 or end > len(array):
+        end = len(array)
+    for i in range(start, end):
+        array[i] = value
+
+
+def find_index(array, predicate, from_index=0):
+    """
+    该方法返回第一个通过 predicate 判断为真值的元素的索引值（index），而不是元素本身
+    返回 index, 默认为 -1
+    predicate 参数依次为 value, index, array
+
+    predicate 如果是 dict
+    :param array:
+    :param predicate: (Array|Function|Object|string)
+    :param from_index:
+    :return:
+    """
+    for i, v in enumerate(array[from_index:]):
+        if isinstance(predicate, (list, dict, str)):
+            if isinstance(predicate, (list, str)):
+                if array.count(predicate) > 0:
+                    return array.index(predicate)
+            else:
+                # 是字典, predicate 可能是 sub dict, 匹配
+                # 匹配 sublist 如下
+                if predicate.items() <= v.items():
+                    return i
+        else:
+            # 说明是函数, 判断参数个数
+                # 判断传入的参数的数量
+            if predicate.__code__.co_argcount == 0:
+                if predicate():
+                    return 0
+                return -1
+            elif predicate.__code__.co_argcount == 1:
+                if predicate(v):
+                    return i
+            elif predicate.__code__.co_argcount == 2:
+                if predicate(v, i):
+                    return i
+            else:
+                if predicate(v, i, array):
+                    return i
+    return -1
+
+
+def find_last_index(array, predicate, from_index=0):
+    """
+    这个方式类似 _.findIndex， 区别是它是从右到左的迭代集合array中的元素
+    :param array:
+    :param predicate:
+    :param from_index:
+    :return:
+    """
+    for i, v in reversed(list(enumerate(array[from_index:]))):
+        if isinstance(predicate, (list, dict, str)):
+            if isinstance(predicate, (list, str)):
+                if array.count(predicate) > 0:
+                    return array.index(predicate)
+            else:
+                # 是字典, predicate 可能是 sub dict, 匹配
+                # 匹配 sublist 如下
+                if predicate.items() <= v.items():
+                    return i
+        else:
+            # 说明是函数, 判断参数个数
+                # 判断传入的参数的数量
+            if predicate.__code__.co_argcount == 0:
+                if predicate():
+                    return -1
+            elif predicate.__code__.co_argcount == 1:
+                if predicate(v):
+                    return i
+            elif predicate.__code__.co_argcount == 2:
+                if predicate(v, i):
+                    return i
+            else:
+                if predicate(v, i, array):
+                    return i
+    return -1
+
+
+def flatten(array):
+    """
+    减少一级array嵌套深度
+    :param array:
+    :return:
+    """
+    result = []
+    for a in array:
+        if type(a) is not list:
+            result.append(a)
+            continue
+        for v in a:
+            result.append(v)
+    return result
+
+
+def flatten_deep(array):
+    """
+    将array递归为一维数组
+    :param array:
+    :return:
+    """
+    result = []
+    for v in array:
+        if isinstance(v, list):
+            result.extend(flatten_deep(v))
+        else:
+            result.append(v)
+    return result
+
+
+def flatten_depth(array, depth=1):
+    """
+    根据 depth 递归减少 array 的嵌套层级
+    :param array:
+    :return:
+    """
+    result = []
+    for v in array:
+        if isinstance(v, list) and depth >= 1:
+            # 控制递归深度
+            result.extend(flatten_depth(v, depth-1))
+        else:
+            result.append(v)
+    return result
+
+
+def from_pairs(paris):
+    """
+    pairs 是一个由 v: k 组成的数组, 这个方法返回一个由键值对pairs构成的字典
+    :param paris:
+    :return:
+    """
+    d = {}
+    for p in paris:
+        d[p[0]] = p[1]
+    return d
+
+
+def head(array):
+    """
+    获取数组 array 的第一个元素
+    :param array:
+    :return:
+    """
+    if len(array) == 0:
+        return None
+    return array[0]
+
+
+def index_of(array, value, from_index=0):
+    """
+    返回首次 value 在数组array中被找到的 索引值
+    :param array:
+    :param value:
+    :param from_index:
+    :return:
+    """
+    try:
+        return array.index(value, from_index)
+    except ValueError:
+        return -1
+
+
+def initial(array):
+    """
+    获取数组array中除了最后一个元素之外的所有元素
+    :param array:
+    :return:
+    """
+    return array[:-1]
+
+
+def intersection(*args):
+    """
+    创建唯一值的数组，这个数组包含所有给定数组都包含的元素
+    :param args:
+    :return:
+    """
+    return list(set(args[0]).intersection(*args))
--- pydash/test/test_array.py
+++ pydash/test/test_array.py
@@ -6,6 +6,20 @@ from main.array import (
     difference_by,
     difference_with,
     drop,
+    drop_right,
+    drop_right_while,
+    drop_while,
+    fill,
+    find_index,
+    find_last_index,
+    flatten,
+    flatten_deep,
+    flatten_depth,
+    from_pairs,
+    head,
+    index_of,
+    initial,
+    intersection,
 )
 import unittest
 import math
@@ -55,11 +69,97 @@ class TestArray(unittest.TestCase):
                              [{'a': 2, 'b': 2}])
 
     def test_drop(self):
-        self.assertListEqual(drop([1, 2, 3, 4, 5],), [2, 3, 4, 5]),
-        self.assertListEqual(drop([1, 2, 3, 4, 5], 1), [2, 3, 4, 5]),
-        self.assertListEqual(drop([1, 2, 3, 4, 5], 2), [3, 4, 5]),
-        self.assertListEqual(drop([1, 2, 3, 4, 5], 5), []),
-        self.assertListEqual(drop([1, 2, 3, 4, 5], 6), []),
+        self.assertListEqual(drop([1, 2, 3, 4, 5], ), [2, 3, 4, 5])
+        self.assertListEqual(drop([1, 2, 3, 4, 5], 1), [2, 3, 4, 5])
+        self.assertListEqual(drop([1, 2, 3, 4, 5], 2), [3, 4, 5])
+        self.assertListEqual(drop([1, 2, 3, 4, 5], 5), [])
+        self.assertListEqual(drop([1, 2, 3, 4, 5], 6), [])
+
+    def test_drop_right(self):
+        self.assertListEqual(drop_right([1, 2, 3, 4, 5], ), [1, 2, 3, 4])
+        self.assertListEqual(drop_right([1, 2, 3, 4, 5], 1), [1, 2, 3, 4])
+        self.assertListEqual(drop_right([1, 2, 3, 4, 5], 2), [1, 2, 3])
+        self.assertListEqual(drop_right([1, 2, 3, 4, 5], 5), [])
+        self.assertListEqual(drop_right([1, 2, 3, 4, 5], 6), [])
+
+    def test_drop_right_while(self):
+        self.assertListEqual(drop_right_while([1, 2, 3, 4, 5], lambda item: item > 3), [1, 2, 3])
+        self.assertListEqual(drop_right_while([1, 2, 3, 4, 5], lambda v, i: v * i > 3), [1, 2])
+
+    def test_drop_while(self):
+        self.assertListEqual(drop_while([1, 2, 3, 4, 5], lambda item: item < 3), [3, 4, 5])
+
+    def test_fill(self):
+        a1 = [1, 2, 3, 4, 5]
+        fill(a1, 0)
+        self.assertListEqual(a1, [0, 0, 0, 0, 0])
+
+        a2 = [1, 2, 3, 4, 5]
+        fill(a2, 0, 2)
+        self.assertListEqual(a2, [1, 2, 0, 0, 0])
+
+        a3 = [1, 2, 3, 4, 5]
+        fill(a3, 0, 2, 3)
+        self.assertListEqual(a3, [1, 2, 0, 4, 5])
+
+        a4 = [1, 2, 3, 4, 5]
+        fill(a4, 0, 0, 8)
+        self.assertListEqual(a4, [0, 0, 0, 0, 0])
+
+    def test_find_index(self):
+        self.assertEqual(find_index(['apple', 'banana', 'beet'], lambda item: item.startswith('b')), 1)
+        self.assertEqual(find_index([{'name': 'apple', 'type': 'fruit'},
+                                     {'name': 'banana', 'type': 'fruit'},
+                                     {'name': 'beet', 'type': 'vegetable'}],
+                                    {'name': 'banana'}),
+                         1)
+        self.assertEqual(find_index(['apple', 'banana', 'beet'], lambda: False), -1)
+
+    def test_find_last_index(self):
+        self.assertEqual(find_last_index(['apple', 'banana', 'beet'], lambda item: item.startswith('b')), 2)
+        self.assertEqual(find_last_index([{'name': 'apple', 'type': 'fruit'},
+                                          {'name': 'banana', 'type': 'fruit'},
+                                          {'name': 'beet', 'type': 'vegetable'}],
+                                         {'type': 'fruit'}),
+                         1),
+        self.assertEqual(find_last_index(['apple', 'banana', 'beet'], lambda: False), -1)
+
+    def test_flatten(self):
+        self.assertEqual(flatten([1, ['2222'], [3, [[4]]]]), [1, '2222', 3, [[4]]])
+
+    def test_flatten_deep(self):
+        self.assertEqual(flatten_deep([1, [2, [3, [4]], 5]]), [1, 2, 3, 4, 5])
+
+    def test_flatten_depth(self):
+        self.assertEqual(flatten_depth([1, [2, [3, [4]], 5]]), [1, 2, [3, [4]], 5])
+        self.assertEqual(flatten_depth([1, [2, [3, [4]], 5]], 2), [1, 2, 3, [4], 5])
+
+    def test_from_pairs(self):
+        self.assertDictEqual(from_pairs([['a', 1], ['b', 2]]), {'a': 1, 'b': 2})
+        self.assertDictEqual(from_pairs([['a', 1], ['b', 2], ['c', 3]]), {'a': 1, 'b': 2, 'c': 3})
+
+    def test_head(self):
+        self.assertEqual(head([1, 2, 3]), 1),
+        self.assertEqual(head([]), None)
+
+    def test_index_of(self):
+        self.assertEqual(index_of([1, 2, 3, 1, 2, 3], 2, 0), 1),
+        self.assertEqual(index_of([1, 2, 3, 1, 2, 3], 2, 3), 4),
+        self.assertEqual(index_of([1, 1, 2, 2, 3, 3], 4, 0), -1),
+        self.assertEqual(index_of([1, 1, 2, 2, 3, 3], 2, 10), -1),
+        self.assertEqual(index_of([1, 1, 2, 2, 3, 3], 0, 0), -1),
+
+    def test_initial(self):
+        self.assertListEqual(initial([1, 2, 3]), [1, 2])
+        self.assertListEqual(initial([1]), [])
+
+    def test_intersection(self):
+        self.assertListEqual(intersection([1, 2, 3], [101, 2, 1, 10], [2, 1]), [1, 2]),
+        self.assertListEqual(intersection([1, 1, 2, 2], [1, 1, 2, 2]), [1, 2]),
+        self.assertListEqual(intersection([1, 2, 3], [4]), []),
+        self.assertListEqual(intersection([1, 2, 3],), [1, 2, 3]),
+        self.assertListEqual(intersection([], [101, 2, 1, 10], [2, 1]), []),
+        self.assertListEqual(intersection([],), [])
 
 
 if __name__ == '__main__':
